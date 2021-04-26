948114bb26dbe18ce9900e3d4ca775bd9b6d728d
Danila Lapko <danlapko@yandex.ru>
2021-04-26 16:54:44 +0300

    update inference video to be written in specified dir

--- supervised_models/inference.py
+++ supervised_models/inference.py
@@ -47,15 +47,21 @@ def video_inference_by_still_frames(vid_path):
     # cv2.destroyAllWindows()
 
 
-def video_inference_by_triplets(model, vid_path, n_frames_to_process=100):
+def video_inference_by_triplets(model, vid_path, n_frames_to_process=100, out_dir=None):
     vid = cv2.VideoCapture(vid_path)
     length = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
     h, w = vid.get(cv2.CAP_PROP_FRAME_WIDTH), vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
     fps = vid.get(cv2.CAP_PROP_FPS)
     print(f"video length: {length} res: {h}x{w} fps: {fps}")
 
-    vid_writer = cv2.VideoWriter(vid_path.replace("video.mov", "out_.avi"), cv2.VideoWriter_fourcc(*'DIVX'), 30,
-                                 tuple(model.input_size[:2] * 2))
+    if out_dir is None:
+        vid_writer = cv2.VideoWriter(vid_path.replace("video.mov", "out_effnet-b0-10.avi"),
+                                     cv2.VideoWriter_fourcc(*'DIVX'), 30,
+                                     tuple(model.input_size[:2] * 2))
+    else:
+        vid_writer = cv2.VideoWriter(os.path.join(out_dir, vid_path.replace("/", "_").replace(".mov", ".avi")),
+                                     cv2.VideoWriter_fourcc(*'DIVX'), 30,
+                                     tuple(model.input_size[:2] * 2))
 
     # preload frames
     ims = []
@@ -89,12 +95,14 @@ if __name__ == '__main__':
     # video_inference_by_still_frames("data/stanford_drone/videos/little/video0/video.mov")
 
     model = StanfordSegModel().load_from_checkpoint(
-        "checkpoints_stanford/deeplabv3_effnet-b2/epoch=16-step=8092.ckpt.backup")
+        "checkpoints_stanford/deeplabv3_effnet-b2-3ch/epoch=22-step=15291.ckpt")
     model.to(device)
     model.setup("val")
     # video_inference_by_triplets(model, "data/stanford_drone/videos/little/video0/video.mov")
+    out_dir = "data/stanford_drone/out"
+    os.makedirs(out_dir, exist_ok=True)
     for i_vid, vid_dir in enumerate(model.val_dataset.video_dirs):
         print("---------------")
         print(i_vid, vid_dir)
         vid_path = os.path.join(vid_dir, "video.mov")
-        video_inference_by_triplets(model, vid_path)
+        video_inference_by_triplets(model, vid_path, n_frames_to_process=1000, out_dir=out_dir)
